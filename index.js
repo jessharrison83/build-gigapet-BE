91319477b1da6e3a603891d1cd2c793c0302f8fc
Delba <delba.brown@gmail.com>
2019-04-15 14:40:30 +0100

    Add parents table

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require("./server/server");
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`\n Server running on http://localhost:${port}`);
+  console.log(`\n Server running on http://localhost:${port} \n`);
 });
