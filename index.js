805dd891195020f268f4942cda03d40fb9dd360c
Delba <delba.brown@gmail.com>
2019-04-15 08:20:53 +0100

    Get API running

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const server = require("./server/server");
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`\n Server running on http://localhost:${port}`);
+});
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.status(200).send(`Welcome to the Gigapet API 🐾`);
+});
+
+module.exports = server;
