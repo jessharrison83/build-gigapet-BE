cd3121d7a069e4f82dcce63f6dc510baf0e9e9bb
mattshardman <mattshardman@gmail.com>
2019-04-15 18:34:28 +0100

    add food routes to server.js

--- server/server.js
+++ server/server.js
@@ -5,12 +5,15 @@ const server = express();
 const { authenticate } = require("../middleware/auth");
 
 const authRoutes = require("../routes/auth");
+const foodEntriesRoutes = require('../foodEntries');
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 server.use("/api/auth", authRoutes);
+
+server.use("/api", foodEntriesRoutes)
 
 server.get("/", (req, res) => {
   res.status(200).send(`Welcome to the Gigapet API 🐾`);
