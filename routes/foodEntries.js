c507a60b6e4836862243c9cee3b827566e5aac13
mattshardman <mattshardman@gmail.com>
2019-04-15 18:32:03 +0100

    add food entries dummy data

--- routes/foodEntries.js
+++ routes/foodEntries.js
@@ -1,3 +1,16 @@
+
+const dummyData = {
+    where: (id) => this.data.filter(each => each.id === id),
+    data: [{
+        id: 1,
+        name: "Porridge",
+        quantity: 2,
+        meal: "Breakfast",
+        category: "Carbs",
+        date: "2019--04-15",
+        child_id: 1
+    }]};
+    
 async function get(id) {
     try {
         const entries = dummyData.where(id);
