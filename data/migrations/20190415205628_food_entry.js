1b53ee78e91bc77ff551f3533a8442397d9a7c17
mattshardman <mattshardman@gmail.com>
2019-04-15 21:07:20 +0100

    create food_entry migration file

--- data/migrations/20190415205628_food_entry.js
+++ data/migrations/20190415205628_food_entry.js
@@ -1,21 +1,21 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable("parents", table => {
+    return knex.schema.createTable("food_entry", table => {
         table.increments();
         table.string("name").notNullable();
         table
             .float("quantity")
-            .notNullable()
-            .unique();
+            .notNullable();
         table
-            .string("username")
-            .notNullable()
-            .unique();
-        table.string("password").notNullable();
-        table.string("img_url");
+            .string("meal")
+            .notNullable();
+        table.string("category").notNullable();
+        table.timestamp("date_added").notNullable();
+        table.timestamp("date_update").notNullable();
+        table.integer("child_id").notNullable();
     });
 };
   
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists("parents");
+    return knex.schema.dropTableIfExists("food_entry");
 };
   
