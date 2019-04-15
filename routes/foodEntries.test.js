e5aa6302b913ea3e94caa85c3cbf5e063f2b4980
mattshardman <mattshardman@gmail.com>
2019-04-15 18:30:49 +0100

    add get food entries route

---
+++ routes/foodEntries.test.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const Food = require("../data/helpers/foodModel"); 
+
+router.get("/child/:id/entries", async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const food = await Food.get(id);
+        res.status(200).json({ entries: food });
+    } catch (error) {
+        next({ status: 500, message: "entry not found"});
+    }
+});
+
+module.exports = router;
