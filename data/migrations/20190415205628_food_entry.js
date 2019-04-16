ac409152aa0603a8ef0aae310002c0927dfb96ed
Delba <delba.brown@gmail.com>
2019-04-16 11:20:26 +0100

    Add foreign key to food entries

--- data/migrations/20190415205628_food_entry.js
+++ data/migrations/20190415205628_food_entry.js
@@ -1,21 +1,22 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable("food_entry", table => {
-        table.increments();
-        table.string("name").notNullable();
-        table
-            .float("quantity")
-            .notNullable();
-        table
-            .string("meal")
-            .notNullable();
-        table.string("category").notNullable();
-        table.timestamp("date_added").notNullable();
-        table.timestamp("date_update").notNullable();
-        table.integer("child_id").notNullable();
-    });
+  return knex.schema.createTable("food_entry", table => {
+    table.increments();
+    table.string("name").notNullable();
+    table.float("quantity").notNullable();
+    table.string("meal").notNullable();
+    table.string("category").notNullable();
+    table.timestamp("date_added").notNullable();
+    table.timestamp("date_update").notNullable();
+    table
+      .integer("child_id")
+      .unsigned()
+      .references("id")
+      .inTable("children")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
 };
-  
+
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists("food_entry");
+  return knex.schema.dropTableIfExists("food_entry");
 };
-  
