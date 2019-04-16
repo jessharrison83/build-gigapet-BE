bd661c76ea4e42c101fa423794a7a5e08886fd72
Delba <delba.brown@gmail.com>
2019-04-16 15:41:25 +0100

    Add more tests for pets

--- routes/pet.test.js
+++ routes/pet.test.js
@@ -1,34 +1,49 @@
-const server = require("../server/server");
 const request = require("supertest");
-// const db = require("../helpers/dbConfig");
+const server = require("../server/server");
+const { generateToken } = require("../middleware/auth");
+
+const user = {
+  id: 1,
+  username: "bob"
+};
+
+const token = generateToken(user);
 
 describe("Pets Endpoint", () => {
-  // let token =
-  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJzdGV2ZSIsImlhdCI6MTU1NTM2MTE5MywiZXhwIjoxNTU1NDQ3NTkzfQ.eBQrBjCE_CZ4alZAsTx1WLemYLpZkPxHImoxqnhvOEU";
-
-  // beforeEach(async () => {
-  //   await db("parents").truncate();
-
-  //   // const response = await request(server)
-  //   //   .post("/api/auth/register")
-  //   //   .send({
-  //   //     name: "name",
-  //   //     email: "email",
-  //   //     username: "username",
-  //   //     password: "password"
-  //   //   });
-
-  //   // token = response.body.token;
-  //   // console.log(response.body);
-  // });
-
-  // afterEach(async () => {
-  //   await db("parents").truncate();
-  // });
-
-  it("Should require authorization", async () => {
+  it("Requires an authorization token", async () => {
     const response = await request(server).get("/api/pet");
-    expect(response.status).toBe(401);
+    expect(response.status).toBe(501);
+  });
+
+  it("Responds with status 200 OK", async () => {
+    const response = await request(server)
+      .get("/api/pet")
+      .set("Authorization", token);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("Returns JSON", async () => {
+    const response = await request(server)
+      .get("/api/pet")
+      .set("Authorization", token);
     expect(response.type).toBe("application/json");
+  });
+
+  it("Returns an array and not another data type", async () => {
+    const response = await request(server)
+      .get("/api/pet")
+      .set("Authorization", token);
+
+    expect(typeof response.body).toBe("object");
+    expect(typeof response.body).not.toBe("string");
+    expect(typeof response.body).not.toBe("number");
+    expect(typeof response.body).not.toBe("boolean");
+    expect(typeof response.body).not.toBe("null");
+    expect(typeof response.body).not.toBe("undefined");
+    expect(typeof response.body).not.toBe("function");
+
+    expect(response.body).not.toEqual({});
+    expect(response.body).toEqual([]);
   });
 });
