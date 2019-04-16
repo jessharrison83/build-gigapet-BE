9db69bbb3376a55753a74a41294700f15ef132f6
Delba <delba.brown@gmail.com>
2019-04-16 21:15:50 +0100

    Run seeds

--- data/seeds/002-children.js
+++ data/seeds/002-children.js
@@ -9,8 +9,8 @@ exports.seed = function(knex, Promise) {
         { id: 2, name: "Jacob", pet_name: "Biscuit", pet_level: 2, pet_id: 1 },
         { id: 3, name: "Alfie", pet_name: "Cosmo", pet_level: 3, pet_id: 1 },
         { id: 4, name: "Olivia", pet_name: "Daisy", pet_level: 1, pet_id: 1 },
-        { id: 5, name: "Amelia", pet_name: "Fig", pet_level: 2, pet_id: 1 }
-        { id: 5, name: "Isabella", pet_name: "Maggie", pet_level: 3, pet_id: 6 }
+        { id: 5, name: "Amelia", pet_name: "Fig", pet_level: 2, pet_id: 1 },
+        { id: 6, name: "Isabella", pet_name: "Maggie", pet_level: 3, pet_id: 6 }
       ]);
     });
 };
