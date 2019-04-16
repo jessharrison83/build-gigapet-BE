018a1f485b123e83b03855d848f8851094b912d5
Delba <delba.brown@gmail.com>
2019-04-16 11:13:22 +0100

    Add parent seeds

--- data/seeds/001-parents.js
+++ data/seeds/001-parents.js
@@ -1,12 +1,28 @@
-// exports.seed = function(knex, Promise) {
-//   // Deletes ALL existing entries
-//   return knex('table_name').del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('table_name').insert([
-//         {id: 1, colName: 'rowValue1'},
-//         {id: 2, colName: 'rowValue2'},
-//         {id: 3, colName: 'rowValue3'}
-//       ]);
-//     });
-// };
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("parents")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("parents").insert([
+        {
+          id: 1,
+          name: "Jeremy",
+          email: "jeremy@jeremy.com",
+          username: "jeremy",
+          password: "jeremy",
+          img_url:
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+        },
+        {
+          id: 2,
+          name: "Steve",
+          email: "steve@steve.com",
+          username: "steve",
+          password: "steve",
+          img_url:
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+        }
+      ]);
+    });
+};
