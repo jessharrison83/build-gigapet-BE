ad3f318e91b82867ab90f6d40b674c84fe520af2
Delba <delba.brown@gmail.com>
2019-04-16 11:18:16 +0100

    Update children's seeds

--- data/seeds/002-children.js
+++ data/seeds/002-children.js
@@ -6,10 +6,10 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex("children").insert([
         { id: 1, name: "Charlie", pet_name: "Acorn", pet_level: 1, pet_id: 1 },
-        { id: 2, name: "Jacob", pet_name: "Biscuit", pet_level: 2, pet_id: 2 },
-        { id: 3, name: "Alfie", pet_name: "Cosmo", pet_level: 3, pet_id: 3 },
+        { id: 2, name: "Jacob", pet_name: "Biscuit", pet_level: 2, pet_id: 1 },
+        { id: 3, name: "Alfie", pet_name: "Cosmo", pet_level: 3, pet_id: 1 },
         { id: 4, name: "Olivia", pet_name: "Daisy", pet_level: 1, pet_id: 1 },
-        { id: 5, name: "Amelia", pet_name: "Fig", pet_level: 2, pet_id: 2 }
+        { id: 5, name: "Amelia", pet_name: "Fig", pet_level: 2, pet_id: 1 }
       ]);
     });
 };
