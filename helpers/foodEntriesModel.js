a19cd17df4c285771abcd80d28a64aa5f910a0b3
mattshardman <mattshardman@gmail.com>
2019-04-17 14:57:07 +0100

    update get food entries to return by meal type

--- helpers/foodEntriesModel.js
+++ helpers/foodEntriesModel.js
@@ -1,55 +1,22 @@
-let idRef = 3;
-
-const dummyData = {
-    data: [{
-        id: 1,
-        name: "Porridge",
-        quantity: 2,
-        meal: "Breakfast",
-        category: "Carbs",
-        date: "2019--04-15",
-        child_id: 1
-    },
-    {
-        id: 2,
-        name: "Soup",
-        quantity: 1,
-        meal: "Lunch",
-        category: "Vegetables",
-        date: "2019--04-15",
-        child_id: 1
-    }],
-    where: (id) => dummyData.data.filter(each => each.child_id == id),
-    insert: (child_id, entry) => {
-        const entryPlusId = { 
-            id: idRef, 
-            child_id, 
-            ...entry 
-        };
-        dummyData.data.push(entryPlusId);
-        idRef++;
-        return dummyData.data; 
-    },
-    update: (id, entry) => {
-        return dummyData.data.map(each => {
-            if (each.id == id) {
-                return {
-                    id: each.id,
-                    ...entry
-                };
-            }
-            return each;
-        });
-    },
-    remove: (id) => dummyData.data.filter(each => each.id != id),
-};
-
 const db = require("./dbConfig");
 
 async function get(id) {
     try {
         const entries = await db("food_entry").where({ child_id: id });
-        return entries;
+        const initialAcc = { 
+            protein: [],
+            carbs: [],
+            vegetables: [],
+            fruit: [],
+            diary: []
+        };
+
+        const sortedEntries = entries.reduce((acc, entry) => {
+            acc[entry.category.toLowerCase()].push(entry);
+            return acc;
+        }, initialAcc);
+
+        return sortedEntries;
     } catch (error) {
         return error;
     }   
@@ -81,7 +48,9 @@ async function add(id, entry) {
 
 async function update(id, entry) {
     try {
-        const entries = dummyData.update(id, entry);
+        const entries = db("food_entry")
+            .where({ id })
+            .update(id, entry);
         return entries;
     } catch (error) {
         return error;
@@ -98,13 +67,26 @@ async function remove(id) {
     }
 }
 
-async function getFilter(query) {
+async function getFilter(id, query) {
     try {
-        const entries = ["working"];
-        return entries;
+        const entries = await db("food_entry").where({ child_id: id });
+        const initialAcc = { 
+            protein: [],
+            carbs: [],
+            vegetables: [],
+            fruit: [],
+            diary: []
+        };
+
+        const sortedEntries = entries.reduce((acc, entry) => {
+            acc[entry.category.toLowerCase()].push(entry);
+            return acc;
+        }, initialAcc);
+
+        return sortedEntries;
     } catch (error) {
         return error;
-    }
+    }   
 }
 
 module.exports = {
