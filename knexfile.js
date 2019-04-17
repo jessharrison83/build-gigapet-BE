687f1402b2422a508455bad6c797d32f9612a780
Delba <delba.brown@gmail.com>
2019-04-17 10:33:17 +0100

    Add production config

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,47 @@
+const dummyPgConfig = {
+  host: "https://lambda-gigapet.herokuapp.com/",
+  database: "gigapet",
+  user: "user",
+  password: "pass"
+};
+
+const productionDbConnection = process.env.DATABASE_URL || dummyPgConfig;
+
 module.exports = {
-    development: {
-        client: "sqlite3",
-        connection: {
-            filename: "./data/gigapet.sqlite3"
-        },
-        useNullAsDefault: true,
-        migrations: {
-            directory: "./data/migrations"
-        },
-        seeds: {
-            directory: "./data/seeds"
-        }
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/gigapet.sqlite3"
     },
-    testing: {
-        client: "sqlite3",
-        connection: {
-            filename: "./data/test.sqlite3"
-        },
-        useNullAsDefault: true,
-        migrations: {
-            directory: "./data/migrations"
-        },
-        seeds: {
-            directory: "./data/seeds"
-        }
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  production: {
+    client: "pg",
+    connection: productionDbConnection,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 };
