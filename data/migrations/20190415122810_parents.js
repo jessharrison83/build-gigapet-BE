37b7aa642e2141527bd5a864b7c05e60117ee73e
Delba <delba.brown@gmail.com>
2019-04-17 07:21:14 +0100

    Add password hash to parent seeds

--- data/migrations/20190415122810_parents.js
+++ data/migrations/20190415122810_parents.js
@@ -1,20 +1,21 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable("parents", table => {
-        table.increments();
-        table.string("name").notNullable();
-        table
-            .string("email")
-            .notNullable()
-            .unique();
-        table
-            .string("username")
-            .notNullable()
-            .unique();
-        table.string("password").notNullable();
-        table.string("img_url");
-    });
+  return knex.schema.createTable("parents", table => {
+    table.increments();
+    table.string("name").notNullable();
+    table
+      .string("email")
+      .notNullable()
+      .unique();
+    table
+      .string("username")
+      .notNullable()
+      .unique();
+    table.string("password").notNullable();
+    table.string("img_url");
+    table.integer("pin_code");
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists("parents");
+  return knex.schema.dropTableIfExists("parents");
 };
