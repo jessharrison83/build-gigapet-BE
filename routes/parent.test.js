52fa8007a3fc2ba343a852b02fe488dd213607d8
Delba <delba.brown@gmail.com>
2019-04-18 10:34:21 +0100

    Update parent test

--- routes/parent.test.js
+++ routes/parent.test.js
@@ -4,53 +4,50 @@ const { generateToken } = require("../middleware/auth");
 
 const db = require("../helpers/dbConfig");
 
-const token = generateToken({ 
-    id: 5,
-    username: "Matt"
+const token = generateToken({
+  id: 5,
+  username: "Matt"
 });
 
 const authHeader = "Authorization";
 
-describe("parent endpoint", () => { 
-    afterEach(async () => {
-        await db("parents").truncate();
-        await db("children").truncate();
-    });
+describe("parent endpoint", () => {
+  afterEach(async () => {
+    await db("parents").truncate();
+    await db("children").truncate();
+  });
 
-    it("returns status code 200", async () => {
-        try {
-            const res = await request(routes)
-                .get("/api/parent/2")
-                .set(authHeader, token);
+  it("returns status code 200", async () => {
+    try {
+      const res = await request(routes)
+        .get("/api/parent/2")
+        .set(authHeader, token);
 
-            expect(res.type)
-                .toBe("application/json"); 
-            expect(res.status) 
-                .toBe(200);
-            expect(res.body)
-                .toEqual({});
-        } catch (error) {
-            console.log(error); //eslint-disable-line
-        }
-    });
+      expect(res.type).toBe("application/json");
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual({});
+    } catch (error) {
+      console.log(error); //eslint-disable-line
+    }
+  });
 });
 
 describe("add child to parent endpoint", () => {
-    it("adds child to children table", async () => {
-        try {
-            const res = await request(routes)
-                .post("/api/parent/1/child")
-                .set(authHeader, token)
-                .send({
-                    "name": "Alfie",
-                    "pet_name": "Alfatron",
-                    "pet_experience": 3,
-                    "pet_id": 1
-                });
+  it("adds child to children table", async () => {
+    try {
+      const res = await request(routes)
+        .post("/api/parent/1/child")
+        .set(authHeader, token)
+        .send({
+          name: "Alfie",
+          pet_name: "Alfatron",
+          pet_experience: 3,
+          pet_id: 1
+        });
 
-            expect(res.status).toBe(201);
-        } catch (error) {
-            console.log(error); //eslint-disable-line
-        }
-    });
+      expect(res.status).toBe(201);
+    } catch (error) {
+      console.log(error); //eslint-disable-line
+    }
+  });
 });
