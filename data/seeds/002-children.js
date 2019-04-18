93e0d2de92cd6d36b100dceeadca33db3e8b61d4
Delba <delba.brown@gmail.com>
2019-04-18 07:18:32 +0100

    Update child seeds

--- data/seeds/002-children.js
+++ data/seeds/002-children.js
@@ -17,28 +17,28 @@ exports.seed = function(knex, Promise) {
           name: "Jacob",
           pet_name: "Biscuit",
           pet_experience: 16,
-          pet_id: 1
+          pet_id: 2
         },
         {
           id: 3,
           name: "Alfie",
           pet_name: "Cosmo",
           pet_experience: 65,
-          pet_id: 1
+          pet_id: 3
         },
         {
           id: 4,
           name: "Olivia",
           pet_name: "Daisy",
           pet_experience: 94,
-          pet_id: 1
+          pet_id: 4
         },
         {
           id: 5,
           name: "Amelia",
           pet_name: "Fig",
           pet_experience: 91,
-          pet_id: 1
+          pet_id: 5
         },
         {
           id: 6,
