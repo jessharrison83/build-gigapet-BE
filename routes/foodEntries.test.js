f2747fab795d2dd458823a6fe6d0692500bd5702
Delba <delba.brown@gmail.com>
2019-04-18 07:18:48 +0100

    Fix food entries test

--- routes/foodEntries.test.js
+++ routes/foodEntries.test.js
@@ -2,30 +2,31 @@ const server = require("../server/server");
 const request = require("supertest");
 const { generateToken } = require("../middleware/auth");
 
-const token = generateToken({ 
-    id: 5,
-    username: "Matt"
+const token = generateToken({
+  id: 5,
+  username: "Matt"
 });
 
 const authHeader = "Authorization";
 
 describe("GET /child/:id/entries", () => {
-    it("returns an array of entries for a given test", async () => {
-        try {
-            const res = await request(server)
-                .get("/api/child/1/entries")
-                .set(authHeader, token);
+  it("returns an array of entries for a given test", async () => {
+    try {
+      const res = await request(server)
+        .get("/api/child/1/entries")
+        .set(authHeader, token);
 
-            expect(res.status).toBe(200);
-            expect(res.body).toEqual({ 
-                "carbs": [],
-                "fruit": [],
-                "protein": [],
-                "vegetables": [],
-                "diary": []
-            });
-        } catch (error) {
-            console.log(error); //eslint-disable-line
-        }
-    });
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual({
+        carbs: [],
+        fruit: [],
+        protein: [],
+        vegetables: [],
+        dairy: [],
+        treats: []
+      });
+    } catch (error) {
+      console.log(error); //eslint-disable-line
+    }
+  });
 });
