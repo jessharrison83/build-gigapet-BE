228774aeee6bf0dbaaf1672d2d9a6ab3aae7e398
mattshardman <mattshardman@gmail.com>
2019-04-19 14:31:25 +0100

    food tests

--- routes/foodEntries.test.js
+++ routes/foodEntries.test.js
@@ -2,6 +2,8 @@ const server = require("../server/server");
 const request = require("supertest");
 const { generateToken } = require("../middleware/auth");
 
+const db = require("../helpers/dbConfig");
+
 const token = generateToken({
     id: 5,
     username: "Matt"
@@ -9,7 +11,22 @@ const token = generateToken({
 
 const authHeader = "Authorization";
 
+async function truncate() {
+    await db("parents").truncate();
+    await db("pets").truncate();
+    await db("children").truncate();
+    await db("food_entry").truncate();
+}
+
+async function seed() {
+    await db.seed.run();
+}
+
 describe("GET /child/:id/entries", () => {
+    beforeEach(seed);
+
+    afterEach(truncate);
+
     it("returns an array of entries for a given test", async () => {
         try {
             const res = await request(server)
@@ -18,14 +35,34 @@ describe("GET /child/:id/entries", () => {
 
             expect(res.status).toBe(200);
             expect(Array.isArray(res));
-            // expect(res.body).toHaveProperty(
-            //     "carbs",
-            //     "fruit",
-            //     "protein",
-            //     "vegetables",
-            //     "dairy",
-            //     "treats"
-            // );
+        } catch (error) {
+            console.log(error); //eslint-disable-line
+        }
+    });
+});
+
+describe("POST /child/:id/entries", () => {
+    beforeEach(seed);
+
+    afterEach(truncate);
+
+    it("adds a food entry", async () => {
+        try {
+            const res = await request(server)
+                .get("/api/child/1/entries")
+                .set(authHeader, token)
+                .send({
+                    name: "Chicken",
+                    quantity: 1,
+                    meal: "Dinner",
+                    category: "Protein",
+                    date_added: "2019-04-11T11:23:09.846Z",
+                    date_update: "2019-04-11T11:23:09.846Z",
+                    child_id: 1
+                });
+
+            expect(res.status).toBe(200);
+            expect(Array.isArray(res));
         } catch (error) {
             console.log(error); //eslint-disable-line
         }
