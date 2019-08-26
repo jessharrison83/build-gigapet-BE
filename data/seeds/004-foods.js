f822f5d59bf0ec3391dd78d0a26fa9f8c093179a
Jess Berler <jessdev83@gmail.com>
2019-08-26 19:43:48 -0400

    fixing seeds

--- data/seeds/004-foods.js
+++ data/seeds/004-foods.js
@@ -1,7 +1,6 @@
 exports.seed = function(knex, Promise) {
     // Deletes ALL existing entries
     return knex("food_entry")
-        .del()
         .then(function() {
             // Inserts seed entries
             return knex("food_entry").insert([
