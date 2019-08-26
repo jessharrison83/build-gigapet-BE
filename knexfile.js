7b5379bb0c00a6e1240953eeebb067dae6b41227
Jess Berler <jessdev83@gmail.com>
2019-08-26 15:51:18 -0400

    postgres prod env added to knexfile

--- knexfile.js
+++ knexfile.js
@@ -33,15 +33,19 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations",
+      // tableName: "dbmigrations"
+    },
+    seeds: { directory: "./data/seeds" }
   }
-  //   production: {
-  //     client: "pg",
-  //     connection: productionDbConnection,
-  //     migrations: {
-  //       directory: "./data/migrations"
-  //     },
-  //     seeds: {
-  //       directory: "./data/seeds"
-  //     }
-  //   }
 };
+
